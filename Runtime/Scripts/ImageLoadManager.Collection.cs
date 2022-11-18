c9b292455c49dc02d9bdf7c8f182d86ab55f2184
Sxer_lmh <308457938@qq.com>
2022-11-18 16:29:23 +0800

    工程上传

---
+++ Runtime/Scripts/ImageLoadManager.Collection.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Sxer.Load.ImageLoader
+{
+    /// <summary>
+    /// 图片资源加载管理
+    /// </summary>
+    public partial class ImageLoadManager
+    {
+        public enum PathType
+        {
+            NewPath,//需要加载
+            DoWaitPath,//不需要加载 需要等待加载完成
+            DoGetPath,//不需要加载 直接获取
+            NullPath//不使用的路径
+        }
+
+       
+
+
+        //获取路径
+
+        //对路径进行检验（路径分三类:新路径(生成资源，并获取)，已存在无资源路径(等待资源再获取)，已存在有资源路径(只获取数据)）
+        
+        
+        /// <summary>
+        /// 资源路径和对应资源实体
+        /// </summary>
+        static Dictionary<string, ImageRes> _resMap = new Dictionary<string, ImageRes>();
+
+        List<ImageRes> imageRes = new List<ImageRes>();
+
+        /// <summary>
+        /// 图片处理工具集合
+        /// </summary>
+        Dictionary<string, ImageLoadHelper> _loadHelperMap = new Dictionary<string, ImageLoadHelper>();
+
+        List<ImagePath> _tempImagePaths = new List<ImagePath>();
+        //Dictionary<string,List<>>
+
+        //生成对应资源类
+
+        //开始下载
+
+        //记录到资源类 //
+
+
+
+        //按资源表加载
+        //按绝对路径加载
+
+        //网络加载
+        //IO加载
+
+
+
+
+        //路径记录表
+        //public List<string> _tempPaths;
+
+        //List<ImageRes> res;
+
+
+
+
+        //单张加载
+        //批量加载
+
+
+        //IO加载
+        //WWW加载
+
+        //通用方式：加载字节数据，(判断图片类型)，转对应图片
+        //特殊方式:WWW直接加载Image 对异常图片没有判断
+
+        //加载效果：每张图开启一个协程去加载
+
+
+        //public static IEnumerator WWWLoadSprite_native(string path, UnityAction<Sprite> action)
+        //{
+
+        //    UnityWebRequest www = UnityWebRequestTexture.GetTexture(path);
+
+        //    yield return www.SendWebRequest();
+
+        //    if (!www.isNetworkError && !www.isHttpError)
+        //    {
+        //        Texture2D texture2D = DownloadHandlerTexture.GetContent(www);
+        //        if (action != null)
+        //            action(Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.zero));
+        //        //清理资源
+        //        texture2D = null;
+        //        www.Dispose();
+        //        Resources.UnloadUnusedAssets();
+        //    }
+        //    else
+        //    {
+        //        Debug.Log(path);
+        //        Debug.LogError(www.error);
+        //        if (action != null)
+        //            action(null);
+        //        //Debug.LogError(www.error);
+        //    }
+
+        //}
+
+
+
+
+
+
+    }
+}
---
+++ Runtime/Scripts/ImageLoadManager.Single.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sxer.Load.ImageLoader
+{
+    //mono单例，场景中只出现一个；调用实例时，场景不存在则生成
+    //包含一些基于mono的事件
+    public partial class ImageLoadManager : MonoBehaviour
+    {
+        private static ImageLoadManager instance;
+        public static ImageLoadManager Instance {
+            get{
+                if (!instance)
+                {
+                    new GameObject("_ImageLoadManager_", typeof(ImageLoadManager));
+                }
+                return instance;
+            }
+        }
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+
+
+        private void OnGUI()
+        {
+            GUILayout.BeginVertical();
+            //资源工具绘制
+            if (_loadHelperMap != null)
+            {
+                GUILayout.BeginVertical();
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("tag");
+                GUILayout.Space(50);
+                GUILayout.Label("state");
+                GUILayout.Space(50);
+                GUILayout.Label("progress");
+                GUILayout.EndHorizontal();
+                foreach (var x in _loadHelperMap)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label(x.Key);
+                    GUILayout.Space(50);
+                    GUILayout.Label(x.Value._helperState.ToString());
+                    GUILayout.Space(50);
+                    GUILayout.Label(x.Value._loadProcess.ToString());
+                    GUILayout.Space(50);
+                    if (x.Value._helperState == HelperState.Ready)
+                    {
+                        if (GUILayout.Button("StartLoad"))
+                        {
+                            x.Value.ResourceLoad();
+                        }
+                    }
+                    
+                    GUILayout.EndHorizontal();
+                }
+
+                GUILayout.EndVertical();
+            }
+
+
+            //if (_resMap != null)
+            //{
+            //    int allcount = _resMap.Count;
+            //    int loadedCount = 0;
+            //    int loadingCOunt = 0;
+
+            //    int line = 0;
+            //    int row = 0;
+            //    foreach (var x in _loadHelperMap.Values)
+            //    {
+            //        foreach (var t in x._imagePaths)
+            //        {
+            //            GUI.DrawTexture(new Rect(0 + 50 * row, 0 + 50 * line, 50, 50), GetImageRes(t.realPath));
+            //            row++;
+            //        }
+            //        row = 0;
+            //        line++;
+            //    }
+            //    //foreach (var x in _resMap.Values)
+            //    //{
+            //    //    GUI.DrawTexture(new Rect(0+ 50* loadedCount, 0, 50, 50), x.resTexture2D);
+            //    //    if (x.m_State == ImageResLoadState.Loading)
+            //    //        loadingCOunt++;
+            //    //    if (x.m_State == ImageResLoadState.Loaded)
+            //    //        loadedCount++;
+            //    //}
+
+            //    GUI.Label(new Rect(100, 100, 200, 200), "总数:" + allcount);
+
+            //    GUI.Label(new Rect(100, 300, 200, 200), "正在加载:" + loadingCOunt);
+            //    GUI.Label(new Rect(100, 500, 200, 200), "加载完成:" + loadedCount);
+            //    foreach (var x in _resMap.Values)
+            //    {
+
+            //    }
+
+            //}
+            GUILayout.EndVertical();
+        }
+
+    }
+
+}
+
